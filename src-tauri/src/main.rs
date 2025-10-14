8995f298a1e1e749b84dc1ed5eabaa81852d717b
PiscesXD <rclu2881@gmail.com>
2025-10-15 03:50:08 +0800

    feat: package

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -170,6 +170,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![scan_directory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
